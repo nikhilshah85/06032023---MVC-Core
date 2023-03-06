3d4e6079562b2f1e9067c061d3b2ba684c49eeb5
Nikhil <nikhilshah.net@hotmail.com>
2023-03-06 13:23:59 +0530

    Day 1 - getting started

---
+++ Day 1/firstMVC/firstMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace firstMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+       
+        public IActionResult EmployeeHome()
+        {
+            return View();
+        }
+
+        public IActionResult EmployeeList()
+        {
+            //collect data from Model file
+            //pass data to view in a ViewBag
+            return View();
+        }
+
+        public IActionResult SearchEmployee()
+        {
+            return View();
+        }
+        public IActionResult AddNewEmployee()
+        {
+            //collect data from View 
+            //pass it to Model
+            return View();
+        }
+    }
+}
