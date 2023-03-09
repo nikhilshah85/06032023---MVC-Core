17b2890022cf1f95a286bb3feed527b9f7a5a4d6
Nikhil <nikhilshah.net@hotmail.com>
2023-03-09 12:57:38 +0530

    Day 3 - EF

---
+++ Day 3/shoppingAPP_MVC/shoppingAPP_MVC/Models/EF/Customer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace shoppingAPP_MVC.Models.EF;
+
+public partial class Customer
+{
+    public int CId { get; set; }
+
+    public string? CName { get; set; }
+
+    public string? CAddress { get; set; }
+
+    public int? CWalletBalance { get; set; }
+
+    public bool? CIsActive { get; set; }
+}
---
+++ Day 3/shoppingAPP_MVC/shoppingAPP_MVC/Models/EF/ShoppingDbMvcContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace shoppingAPP_MVC.Models.EF;
+
+public partial class ShoppingDbMvcContext : DbContext
+{
+    public ShoppingDbMvcContext()
+    {
+    }
+
+    public ShoppingDbMvcContext(DbContextOptions<ShoppingDbMvcContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("server=(localdb)\\MSSQLLocalDB;database=shoppingDB_MVC;integrated security=true");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CId).HasName("PK__customer__D830D4772358802D");
+
+            entity.ToTable("customers");
+
+            entity.Property(e => e.CId)
+                .ValueGeneratedNever()
+                .HasColumnName("cId");
+            entity.Property(e => e.CAddress)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("cAddress");
+            entity.Property(e => e.CIsActive).HasColumnName("cIsActive");
+            entity.Property(e => e.CName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("cName");
+            entity.Property(e => e.CWalletBalance).HasColumnName("cWalletBalance");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.Pid).HasName("PK__Products__DD37D91AB209622E");
+
+            entity.Property(e => e.Pid)
+                .ValueGeneratedNever()
+                .HasColumnName("pid");
+            entity.Property(e => e.PCategory)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("pCategory");
+            entity.Property(e => e.PIsInStock).HasColumnName("pIsInStock");
+            entity.Property(e => e.PName)
+                .HasMaxLength(20)
+                .IsUnicode(false)
+                .HasColumnName("pName");
+            entity.Property(e => e.PPrice).HasColumnName("pPrice");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
