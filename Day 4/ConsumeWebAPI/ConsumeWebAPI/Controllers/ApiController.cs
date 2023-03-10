043585e90d456ecd2fedb9b436fc6c8d22cfa043
Nikhil <nikhilshah.net@hotmail.com>
2023-03-10 13:23:36 +0530

    Day 4 - API call client side

---
+++ Day 4/ConsumeWebAPI/ConsumeWebAPI/Controllers/ApiController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsumeWebAPI.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult Postdata()
+        {
+            return View();
+        }
+    }
+}
