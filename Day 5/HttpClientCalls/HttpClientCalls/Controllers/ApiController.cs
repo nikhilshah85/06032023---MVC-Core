9cb09cb5bd42a1dbd3e85c53f0ae47a7e98b423e
Nikhil <nikhilshah.net@hotmail.com>
2023-03-14 13:29:59 +0530

    Day 5 - HttpClient Call

---
+++ Day 5/HttpClientCalls/HttpClientCalls/Controllers/ApiController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HttpClientCalls.Models;
+namespace HttpClientCalls.Controllers
+{
+    public class ApiController : Controller
+    {
+
+
+        //PostDataModel pObj = new PostDataModel(); 
+        //u should use DI here
+
+        PostDataModel _pObj;
+
+        public ApiController(PostDataModel _pObjREF)
+        {
+            _pObj = _pObjREF;
+        }
+
+        public IActionResult PostData()
+        {
+            ViewBag.post = _pObj.GetPostData();
+            return View();
+        }
+    }
+}
---
+++ Day 5/HttpClientCalls/HttpClientCalls/Models/PostDataModel.cs
@@ -0,0 +1,39 @@
+﻿namespace HttpClientCalls.Models
+{
+    public class PostDataModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+
+        static List<PostDataModel> pList = new List<PostDataModel>();
+
+        public List<PostDataModel> GetPostData()
+        {
+            string url = "https://jsonplaceholder.typicode.com/posts";
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Clear(); // clear the default type from the calling environment
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+            var makeCall = client.GetAsync(url).Result; //make a call 
+
+            //check if call is getting successful, else throw exception
+            if (makeCall.IsSuccessStatusCode)
+            {
+                var readData = makeCall.Content.ReadAsAsync<List<PostDataModel>>(); //this line  show error because we need to add a nuget package 
+                readData.Wait();
+                pList = readData.Result;              
+
+            }
+            else
+            {
+                throw new Exception("Call failed, please contact Admin");
+            }
+
+
+            return pList; 
+
+        }
+    }
+}
