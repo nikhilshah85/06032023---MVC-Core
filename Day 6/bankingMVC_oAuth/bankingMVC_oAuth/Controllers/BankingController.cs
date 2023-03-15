cb0db79bf14050718d56215f41e33815c3c5528c
Nikhil <nikhilshah.net@hotmail.com>
2023-03-15 13:26:10 +0530

    oAuth

---
+++ Day 6/bankingMVC_oAuth/bankingMVC_oAuth/Controllers/BankingController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bankingMVC_oAuth.Controllers
+{
+
+    [Authorize]
+    public class BankingController : Controller
+    {
+
+        [AllowAnonymous]
+        public IActionResult BankingHome()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Services()
+        {
+            return View();
+        }
+        //user should be logged in
+        public IActionResult DownloadStatement()
+        {
+            return View();
+        }
+        //user should be logged in
+        public IActionResult TransferFunds()
+        {
+            return View();
+        }
+    }
+}
