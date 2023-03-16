22c339eba85a467a8af46aa26e88466213c92e6d
Nikhil <nikhilshah.net@hotmail.com>
2023-03-16 13:24:14 +0530

    CORS

---
+++ Day 7/consumeAPI/consumeAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace consumeAPI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Employee()
+        {
+            return View();
+        }
+    }
+}
